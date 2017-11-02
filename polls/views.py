a570b0d3f100290a1e97be8876173f7ca0b667a1
Du Chao <chao_du@cyai.com>
2017-11-02 13:49:42 +0800

    debug

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.shtml'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
